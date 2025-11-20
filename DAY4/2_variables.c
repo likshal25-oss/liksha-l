5c7df4b6e85389d41bbcf7360610867aed641af7
likshal25-oss <likshal25@gmail.com>
2025-11-20 15:49:25 +0530

    day4 code pushed

---
+++ DAY4/2_variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age = 30;
+    printf("your age is: %d" , age);
+    float marks = 345.34;
+    printf("your marks is: %f", marks);
+    double pi = 3.14;
+    printf("value of pi is: %lf",pi);
+    char gender = 'M';
+    printf("\n your \? gender is: %c",gender);
+    bool isWalking = false;
+    printf("\n Is\" Walk\ting \": %d", isWalking);
+}
