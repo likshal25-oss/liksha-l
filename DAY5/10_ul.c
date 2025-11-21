ca02308c11a9cf934553d5356d04dd44ecbbda26
likshal25-oss <likshal25@gmail.com>
2025-11-21 15:28:46 +0530

    day 5 code pushed

---
+++ DAY5/10_ul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character: ");
+    scanf("%c",&ch);
+    if(ch>=65 && ch<=90)
+    { printf("Captial letter");
+    }
+    else
+    { printf("small letter");
+    }    
+}
---
+++ DAY5/11_forloop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("numbers from 1 to 10: \n");
+    for(int i = 1; i<= 100;i++){
+        printf("%d",i);
+    }
+}
